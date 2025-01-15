76e81ec38f820bad6d1825482ccc6a4ee018a9f3
CyberSportsmen <alextinica007@gmail.com>
2025-01-15 20:19:01 +0200

    curcubeuuu

--- Resurse/Random.js
+++ Resurse/Random.js
@@ -3,7 +3,6 @@ const butonulMeuFermecat = document.getElementById("horror");
 butonulMeuFermecat.addEventListener("mouseenter", (eveniment) => {
   const copil = butonulMeuFermecat.children[0];
 
-  // nu mai adauga settimeout daca exista deja unul
   if (!copil.classList.contains("invarte")) {
     copil.classList.add("invarte");
 
@@ -12,3 +11,21 @@ butonulMeuFermecat.addEventListener("mouseenter", (eveniment) => {
     }, 2000);
   }
 });
+
+const linkRickroll = document.querySelector(".rickroll");
+
+function genereazaCuloareRandom() {
+  const litere = "0123456789ABCDEF";
+  let culoare = "#";
+  for (let i = 0; i < 6; i++) {
+    culoare += litere[Math.floor(Math.random() * 16)];
+  }
+  return culoare;
+}
+
+function schimbaCuloare() {
+  const culoareNoua = genereazaCuloareRandom();
+  linkRickroll.style.color = culoareNoua;
+}
+
+setInterval(schimbaCuloare, 1500);
